02df7af97fd2240cdd7350f67fb1ea1ce38b5ccc
hdt <ngdathd@gmail.com>
2019-05-07 23:53:32 +0700

    first

---
+++ app/src/main/java/com/trantri/tdt_music/Fragment/baocao/IBaoCaoContract.java
@@ -0,0 +1,18 @@
+package com.trantri.tdt_music.Fragment.baocao;
+
+import java.util.List;
+
+public interface IBaoCaoContract {
+    interface IView {
+        void showSongs(List<Song> songs1,
+                       List<Song> songs2,
+                       List<Song> songs3,
+                       List<Song> songs4,
+                       List<Song> songs5
+        );
+    }
+
+    interface IPresenter {
+        void getSongs();
+    }
+}
